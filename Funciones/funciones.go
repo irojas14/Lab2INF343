b84b515afc5f92a7519334e1ddc514cefcb30d89
Maximiliano Bombin <maximiliano.bombin@gmail.com>
2021-10-31 06:11:57 -0300

    Intento de importación de funciones

--- Funciones/funciones.go
+++ Funciones/funciones.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func random_in_range(min int32, max int32) int32 {
+func RandomInRange(min int32, max int32) int32 {
 	rand.Seed(time.Now().UnixNano())
 	return rand.Int31n(max-min+1) + min
 }
--- Funciones/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/irojas14/Lab2INF343/Funciones
-
-go 1.17
